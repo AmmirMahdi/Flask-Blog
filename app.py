5b2cf7851958a63ad3db98dbda4d00d8f55b11da
Edward <a.e.abbas1379@gmail.com>
2020-03-06 14:57:11 +0330

    key update

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'blog.sqlite')
 app.config['SQL_TRACK_MODIFICATIONS'] = False 
-app.config['SECRET_KEY'] = 'amirmahdirashvnad5329@@@@'
+app.config['SECRET_KEY'] = 'ABCDEFGHIJK12345#@$%'
 
 db = SQLAlchemy(app)
 
